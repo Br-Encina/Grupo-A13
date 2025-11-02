591d9fe641d5e00d1b03de4539ac0ce7702cacf3
Ivan <ivan_prat_1995@hotmail.com.ar>
2025-11-01 23:23:38 -0300

    Enemigo nuevo + Animacion

--- Assets/Franco/EnemyScripts/EnemyMelee.cs
+++ Assets/Franco/EnemyScripts/EnemyMelee.cs
@@ -1,74 +1,64 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.UIElements.Experimental;
-using static UnityEngine.GraphicsBuffer;
 
 public class EnemyMelee : Enemy
 {
-    [SerializeField] private float forceAtack = 50f;
-    //[SerializeField] private Animator animator;
-    private Vector3 direction;
-    [SerializeField]  private float retroceso=5f;
+  [SerializeField] private float forceAtack = 25f;
+  private Animator animator;
+  private Vector3 direction;
+  [SerializeField] private float retroceso = 5f;
 
-    [Header("Warning Area")]
-    [SerializeField] private GameObject warning; 
-    [SerializeField] private float warningDuration = 0.5f;
-   
-   
+  [Header("Warning Area")]
+  [SerializeField] private GameObject warning;
+  [SerializeField] private float warningDuration = 0.5f;
+    
+  private void Start()
+  {
+    warning.SetActive(false);
+    animator = GetComponent<Animator>();
+  }
 
+  public override void StateAtacar()
+  {
 
-    private void Start()
+    if (!live) return;
+    StartCoroutine(ShowWarningArea(target.transform.position));
+    if (PuedeAtacar())
     {
-       warning.SetActive(false);
+      // La direcciï¿½n se sigue usando para el impulso
+      direction = (target.transform.position - transform.position).normalized;
+      // ï¿½IMPORTANTE! Ahora pasamos la posiciï¿½n del objetivo (target.transform.position)
+      animator.SetTrigger("IsAttack");
+      rb.AddForce((direction * forceAtack), ForceMode.Impulse);
 
+      ReiniciarCooldown();
     }
-
-
-    public override void StateAtacar()
+  }
+  private void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.CompareTag("Player"))
     {
-
-        if (!live) return;
-        StartCoroutine(ShowWarningArea(target.transform.position));
-        if (PuedeAtacar())
-        {
-            // La dirección se sigue usando para el impulso
-            direction = (target.transform.position - transform.position).normalized;
-
-            // ¡IMPORTANTE! Ahora pasamos la posición del objetivo (target.transform.position)
-            
-
-            rb.AddForce((direction * forceAtack), ForceMode.Impulse);
-
-            ReiniciarCooldown();
-        }
+      target.GetComponent<Rigidbody>().AddForce(Vector3.up * 5f, ForceMode.Impulse);
+      Debug.Log("Jugador golpeado por el enemigo cuerpo a cuerpo");
+      healthManager.TakeDamage(10);
     }
-
-    private void OnCollisionEnter(Collision collision)
+    else if (collision.gameObject.CompareTag("Pared"))
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            
-            target.GetComponent<Rigidbody>().AddForce(Vector3.up * 5f, ForceMode.Impulse);
-            Debug.Log("Jugador golpeado por el enemigo cuerpo a cuerpo");
-            healthManager.TakeDamage(10);
-        }
-        else if (collision.gameObject.CompareTag("Pared"))
-        {
-            Debug.Log("Enemigo cuerpo a cuerpo tocó pared");
+      Debug.Log("Enemigo cuerpo a cuerpo tocï¿½ pared");
 
-            Vector3 normal = collision.contacts[0].normal;
+      Vector3 normal = collision.contacts[0].normal;
 
-            Vector3 bounceDirection = Vector3.Reflect(direction, normal).normalized;
+      Vector3 bounceDirection = Vector3.Reflect(direction, normal).normalized;
 
-            rb.linearVelocity = Vector3.zero;
+      rb.linearVelocity = Vector3.zero;
 
-            rb.AddForce(bounceDirection * retroceso, ForceMode.Impulse);
+      rb.AddForce(bounceDirection * retroceso, ForceMode.Impulse);
 
-            ChangeState(States.idle);
+      ChangeState(States.idle);
 
-            StartCoroutine(DelayStun(0.1f));
-        }
+      StartCoroutine(DelayStun(0.1f));
     }
+  }
     // abria que hacer una interfaz IStunnable
     private IEnumerator DelayStun(float delay)
     {
@@ -76,26 +66,26 @@ public class EnemyMelee : Enemy
         ApplyStun();
     }
 
-    // abria que hacer una interfaz ShowWarning
-    private IEnumerator ShowWarningArea(Vector3 targetPosition)
-    {
-       
-        Vector3 direccionAlObjetivo = targetPosition - warning.transform.position;
+  // abria que hacer una interfaz ShowWarning
+  private IEnumerator ShowWarningArea(Vector3 targetPosition)
+  {
 
-        
-        Quaternion rotacionDeseada = Quaternion.LookRotation(direccionAlObjetivo);
+    Vector3 direccionAlObjetivo = targetPosition - warning.transform.position;
 
-       
-        float anguloY = rotacionDeseada.eulerAngles.y;
 
-        
-        warning.transform.rotation = Quaternion.Euler(0f, anguloY, 0f);
+    Quaternion rotacionDeseada = Quaternion.LookRotation(direccionAlObjetivo);
 
-      
-        warning.SetActive(true);
-       
-        yield return new WaitForSeconds(warningDuration);
 
-        warning.SetActive(false);
-    }
+    float anguloY = rotacionDeseada.eulerAngles.y;
+
+
+    warning.transform.rotation = Quaternion.Euler(0f, anguloY, 0f);
+
+
+    warning.SetActive(true);
+
+    yield return new WaitForSeconds(warningDuration);
+
+    warning.SetActive(false);
+  }
 }
